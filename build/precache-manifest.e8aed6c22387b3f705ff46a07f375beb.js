12f8cb6af8fef1a5095ee159e77084bd3eb02f42
Potapov Aleksey <alex.potap1811@gmail.com>
2019-06-02 21:12:37 +0300

    finish hw4

---
+++ build/precache-manifest.e8aed6c22387b3f705ff46a07f375beb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be141264ef729580b896",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.9bbf1db5.js"
+  },
+  {
+    "revision": "b7c6e28028ca5afb6acf",
+    "url": "/goit-react-hw-04-reader/static/js/main.7c2c9b26.chunk.js"
+  },
+  {
+    "revision": "7a3e7268f4b08f6d4a3b",
+    "url": "/goit-react-hw-04-reader/static/js/2.68d99b52.chunk.js"
+  },
+  {
+    "revision": "b7c6e28028ca5afb6acf",
+    "url": "/goit-react-hw-04-reader/static/css/main.6a00f19b.chunk.css"
+  },
+  {
+    "revision": "be04b1921cc7d2bbb4b61fc9db899f45",
+    "url": "/goit-react-hw-04-reader/index.html"
+  }
+];
